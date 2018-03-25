2fc30d7384c2783ce014dbf83fb30a79c82acfbd
baconadmiral <andrewwilliams1087@gmail.com>
2018-03-25 17:52:28 -0600

    Added Game Restarting and Tries Tracking

--- sketch.js
+++ sketch.js
@@ -1,54 +1,74 @@
 var bird;
 var pipes = [];
 var score;
+var highScore;
 var crashes;
+var tries;
+var gameStarted;
 
 
 function setup() {
   createCanvas(400, 600);
   bird = new Bird();
-  score = 0;
-  crashes = 0;
+  this.score = 0;
+  this.highScore = 0;
+  this.tries = 0;
+  this.crashes = 0;
   pipes.push(new Pipe());
   this.flappyBak = loadImage('flappybak.png');
-  //angleMode(DEGREES);
+  this.gameStarted = false;
 }
 
 function draw() {
   background(flappyBak);
 
-  textSize(32);
-  text('Score: ' + score + " Crashes: " + crashes, 40, 30);
-  fill(0, 102, 153, 51);
-
-  for(var i = pipes.length-1; i > 0; i--)
+  if(!this.gameStarted)
   {
-    pipes[i].showPipe();
-    pipes[i].update();
-
-    if(pipes[i].hits(bird))
-    {
-      crashes += 1;
-    }
-
-    if(pipes[i].score(bird))
-    {
-      score += 1;
-    }
-
-    if(pipes[i].x < -50 )
-    {
-      pipes.splice(i, 1);
-    }
+    pipes = [];
+    pipes.push(new Pipe());
+    textSize(32);
+    text("Press Any Key to Start", 40, height/2);
+    fill(0);
   }
 
+  if(this.gameStarted)
+  {
+    textSize(32);
+    text('Score: ' + this.score + " High: " + this.highScore, 40, 30);
+    text('Tries: ' + this.tries, 40, 70);
+    fill(0, 102, 153, 51);
 
-  bird.showBird();
-  bird.update();
+    for(var i = pipes.length-1; i > 0; i--)
+    {
+      pipes[i].showPipe();
+      pipes[i].update();
 
-  if(frameCount % 100 == 0)
-    pipes.push(new Pipe());
+      if(pipes[i].hits(bird))
+      {
+        this.gameStarted = false;
+        this.highScore = this.score;
+        this.tries += 1;
+        this.score = 0;
+      }
 
+      if(pipes[i].score(bird))
+      {
+        this.score += 1;
+      }
+
+      if(pipes[i].x < -50 )
+      {
+        pipes.splice(i, 1);
+      }
+    }
+
+
+    bird.showBird();
+    bird.update();
+
+    if(frameCount % 100 == 0)
+      pipes.push(new Pipe());
+  }
 
 }
 
@@ -60,6 +80,9 @@ function mouseClicked()
 
 function keyPressed()
 {
+  if(!this.gameStarted)
+    this.gameStarted = true;
+
   if(key == " ")
   {
     bird.bounce();
